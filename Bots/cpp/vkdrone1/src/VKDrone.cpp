ed97391f5218247df3e0beacb15c45aba04ea141
Vinícius <vkdaros@ime.usp.br>
2015-08-25 23:46:17 -0300

    Bot trying to turn to face enemy ship. Target angle is ok, but better thurst control is needed.

--- Bots/cpp/vkdrone1/src/VKDrone.cpp
+++ Bots/cpp/vkdrone1/src/VKDrone.cpp
@@ -3,6 +3,14 @@
 
 #include "VKDrone.h"
 
+using std::map;
+
+using std::pow;
+using std::sqrt;
+using std::sin;
+using std::cos;
+using std::acos;
+
 VKDrone::VKDrone() {
 }
 
@@ -10,24 +18,35 @@ VKDrone::~VKDrone() {
 }
 
 void VKDrone::Process() {
-    int target = -2;
-    target = closestShip(gameState->ships);
-    gameState->Log("uid: " + std::to_string(target) +
-                   " x: "  + std::to_string(gameState->ships[target]->posx) +
-                   " y: "  + std::to_string(gameState->ships[target]->posy));
+    Ship *target = closestShip(gameState->ships);
+    if (target == NULL) {
+        // Do nothing when there is no enemies left.
+        return;
+    }
+    /*
+    gameState->Log("x: "  + to_string(target->posx) +
+                   " y: " + to_string(target->posy));
+    */
 
-    //sideThrustFront = 1.0f;
-    //sideThrustBack = 1.0f;
+    // Rotate to face the target.
+    aimAt(target);
+    thrust = 0.01f;
+
+    /*
+    gameState->Log("ang: "  + to_string(target->ang) +
+                   "; va: " + to_string(target->velAng));
+    */
 }
 
-int VKDrone::closestShip(std::map<int, Ship*> &ships) {
+Ship* VKDrone::closestShip(map<int, Ship*> &ships) {
     // Squared distance to closest enemy ship.
     float shortestDist2 = -1;
 
     // ID of closest enemy ship.
-    int uid = -1;
+    // Initial value just to check later if any enemy was found.
+    int uid = myShip->uid;
 
-	for(std::map<int, Ship*>::iterator itr = ships.begin();
+	for(map<int, Ship*>::iterator itr = ships.begin();
         itr != ships.end(); itr++) {
 
         Ship *ship = itr->second;
@@ -38,8 +57,8 @@ int VKDrone::closestShip(std::map<int, Ship*> &ships) {
         }
 
         // Squared distance to enemy ship.
-        float dist2 = std::pow(ship->posx - myShip->posx, 2) +
-                      std::pow(ship->posy - myShip->posy, 2);
+        float dist2 = pow(ship->posx - myShip->posx, 2) +
+                      pow(ship->posy - myShip->posy, 2);
 
         // Update closest enemy information.
         if (dist2 < shortestDist2 || shortestDist2 <= 0) {
@@ -48,5 +67,51 @@ int VKDrone::closestShip(std::map<int, Ship*> &ships) {
         }
     }
 
-    return uid;
+    if (uid == myShip->uid) {
+        // No enemy found.
+        return NULL;
+    }
+    return gameState->ships[uid];
+}
+
+void VKDrone::aimAt(const GameObject *obj) {
+    return aimAt(obj->posx, obj->posy);
+}
+
+void VKDrone::aimAt(float x, float y) {
+    // Angle from NORTH to "ang" attribute restricted to [-2PI, 2PI].
+    float facingAngle = fmod(myShip->ang, 360) * (PI / 180.0f);
+
+    // Ajust facingAngle to [-PI, PI]. West -> negative, East -> positive.
+    if (facingAngle > PI) {
+        facingAngle -= 2 * PI;
+    } else if (facingAngle < -PI) {
+        facingAngle += 2 * PI;
+    }
+
+    // Unitary vector of facing direction from NORTH.
+    float facingX = -1 * sin(facingAngle);
+    float facingY = cos(facingAngle);
+
+    // Vector to target with respect to myShip.
+    float targetX = x - myShip->posx;
+    float targetY = y - myShip->posy;
+
+    // Find the angle alpha between the vectors using "dot (or inner) product".
+    // Note: alpha is in [0,PI] because of acos.
+    float alpha = acos((facingX * targetX + facingY * targetY) /
+                       (
+                           sqrt(facingX * facingX + facingY * facingY) *
+                           sqrt(targetX * targetX + targetY * targetY)
+                       ));
+
+    // Cross product sign tells if target is at right (negative alpha).
+    if (facingX * targetY - facingY * targetX < 0) {
+        alpha *= -1.0f;
+    }
+
+    gameState->Log("facing: " + to_string(facingAngle) + " rad: " + to_string(alpha));
+
+    sideThrustFront = alpha * -0.1f;
+    sideThrustBack = -sideThrustFront;
 }
--- Bots/cpp/vkdrone1/src/VKDrone.h
+++ Bots/cpp/vkdrone1/src/VKDrone.h
@@ -1,6 +1,8 @@
 #ifndef VKDrone_H
 #define VKDrone_H
 
+#define PI (3.14159265f)
+
 #include <map>
 
 #include "BotBase.h"
@@ -13,8 +15,12 @@ class VKDrone : public BotBase {
     virtual void Process();
 
   protected:
-    // Return uid of closest enemy ship.
-    int closestShip(std::map<int, Ship*> &ships);
+    // Return uid of closest enemy ship or NULL when no enemy was found.
+    Ship* closestShip(std::map<int, Ship*> &ships);
+
+    // Rotates the ship to face a object or a specific coordinate.
+    void aimAt(const GameObject *obj);
+    void aimAt(float x, float y);
 };
 
 #endif
